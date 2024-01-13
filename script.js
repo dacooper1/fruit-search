04ad4d31ea3d88993ec0f45199f1e72f4379244d
Dejah Cooper <Dejah.C@icloud.com>
2024-01-12 19:02:51 -0500

    search function completed

---
+++ script.js
@@ -0,0 +1,39 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+let searching = function search(str) {
+	let results = [];
+
+	// TODO
+	for (let item of fruit){
+		let lowerItem = item.toLowerCase()
+		let lowerStr = str.toLowerCase()
+		if (lower.includes(str)) {
+			results.push(item)
+		}
+	}
+	return results;
+}
+
+function searchHandler(search) {
+	searching()
+	for (let item of search) {
+		let newLi = document.createElement('li')
+		suggestions.append(newLi)
+		newLi.innerText = item
+	}
+}
+
+function showSuggestions(results, inputVal) {
+
+	// TODO highlights over the hovered fruit
+}
+
+function useSuggestion(e) {
+	// TODO adds selected fruit to search bar
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
